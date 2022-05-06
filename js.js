215cd9ae6a95a45bb624186f658c122f0987a807
wallisinaraujo0 <wallisonaraujo0@gmail.com>
2022-05-06 10:50:07 -0300

    transformando numero em string e vice-versa

---
+++ js.js
@@ -0,0 +1,15 @@
+let a = "20";
+let b = "10";
+let c = parseInt(a) + parseInt(b);
+let d = 10;
+let e = 20;
+let f = d.toString() + e.toString();
+const g = {
+    name:'wallison',
+    lastName:'araujo',
+    age:29
+}
+
+console.log(`transform sttring in number, the sum of the numbers is  ${c} typeof ${typeof(c)}`);
+console .log(`transform number in string, the concatenation of the strings is ${f} tipeof ${typeof(f)}`)
+
